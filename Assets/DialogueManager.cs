beb7109a3371f5f7a59e25a3d40eed241b0289f3
cs21b052 <110807579+cs21b052@users.noreply.github.com>
2024-01-16 11:58:23 +0530

    Intro added

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text messageText;
+    public Text actorName;
+    public Button continueButton;
+    public RectTransform backgroundBox;
+
+    Message[] currentMessages;
+    Actor[] currentActors;
+    int activeMessage = 0;
+
+    public void StartDialogue(Message[] messages, Actor[] actors)
+    {
+        currentMessages = messages;
+        currentActors = actors;
+        activeMessage = 0;
+
+        Debug.Log("Starting dialogue with " + currentMessages.Length + " messages.");
+        DisplayMessage();
+        backgroundBox.LeanScale(new Vector3(5.373f, 1.53f, 1.30f), 0.5f).setEaseInOutExpo();
+
+    }
+
+    void DisplayMessage()
+    {
+        Message messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.message;
+
+        Actor actorToDisplay = currentActors[messageToDisplay.actorid];
+        actorName.text = actorToDisplay.name;
+
+        AnimateTextColor();
+    }
+
+    public void NextMessage()
+    {
+        activeMessage++;
+        if (activeMessage >= currentMessages.Length)
+        {
+            Debug.Log("No more messages.");
+        
+            NextScene();
+            return;
+        }
+        DisplayMessage();
+    }
+
+    void AnimateTextColor()
+    {
+        LeanTween.textAlpha(messageText.rectTransform, 0, 0);
+        LeanTween.textAlpha(actorName.rectTransform, 0, 0);
+        LeanTween.textAlpha(messageText.rectTransform, 1, 0.5f).setEaseInOutExpo();
+        LeanTween.textAlpha(actorName.rectTransform, 1, 0.5f).setEaseInOutExpo();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundBox.transform.localScale = Vector3.zero;
+        continueButton.onClick.AddListener(NextMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get element by name -> button -> onclick -> add dialogue manager -> next message
+            
+    }
+
+    public void NextScene()
+    {
+        Debug.Log("Next scene");
+        SceneManager.LoadScene("Lvl-1");
+    }
+}
