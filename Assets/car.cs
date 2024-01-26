f2d81d5c2a5bc096253da63aa8ac2b89af7c5b47
sai <110807333+sai110304@users.noreply.github.com>
2024-01-26 13:29:56 +0530

    level_O

---
+++ Assets/car.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    public Rigidbody2D carRigidbody;
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+    public float speed = 20;
+    public float carTorque = 10;
+    private float movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+    }
+
+    public void FixedUpdate()
+    {
+        backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        carRigidbody.AddTorque(-movement*carTorque*Time.fixedDeltaTime);
+    }
+}
+ 
