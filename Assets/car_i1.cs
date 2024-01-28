dd5c6e17c55205db0f01f44bcfa8b14c215cc3b3
sai <110807333+sai110304@users.noreply.github.com>
2024-01-28 16:15:25 +0530

    I_Lvl2

--- Assets/car_i1.cs
+++ Assets/car_i1.cs
@@ -9,36 +9,127 @@ public class car_i1 : MonoBehaviour
     public Rigidbody2D frontTire;
     public float speed = 20;
     public float carTorque = 10;
-    public float maxSlopeAngle = 10;
     private float movement;
+
     public GameObject para;
-    public Vector3 offset;
-    //public int booster = 1;
-    // Start is called before the first frame update
+
+    private float parachuteOffsetX = -6f;
+    private float parachuteOffsetY = 2f;
+    private float parachuteOffsetZ = 0f;
+
+    private bool parachuteDeployed = false;
+    private GameObject deployedParachute;
+
+
+    private bool isGrounded = false;
+
     void paraStart()
     {
-        Transform temp = transform;
-        GameObject parashute = Instantiate(para,temp);
-        parashute.transform.localScale = new Vector3(2000, 1000, 0);
-    }
-    void Start()
-    {
-        paraStart();
+
+        Vector3 carTopPoint = transform.position + new Vector3(parachuteOffsetX, parachuteOffsetY, parachuteOffsetZ);
+
+     
+        deployedParachute = Instantiate(para, carTopPoint, Quaternion.identity);
+        deployedParachute.transform.localScale = new Vector3(5, 5, 1);
+        deployedParachute.transform.parent = transform;
+
+        parachuteDeployed = true;
+
+
+        carRigidbody.freezeRotation = true;
     }
 
-    // Update is called once per frame
+
+    void DismantleParachute()
+    {
+        if (deployedParachute != null)
+        {
+            Destroy(deployedParachute);
+            deployedParachute = null;
+            parachuteDeployed = false;
+
+       
+            carRigidbody.freezeRotation = false;
+        }
+    }
+
+
+
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.P) && !parachuteDeployed)
+        {
+            paraStart();
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && !parachuteDeployed)
+        {
+            paraStart();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && parachuteDeployed)
+        {
+            DismantleParachute();
+        }
     }
 
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            DismantleParachute();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
 
     public void FixedUpdate()
     {
+        float tireRotationReductionFactor = 0.3f;
 
-        backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
-        frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        float tireRotationTorque = -movement * speed * Time.fixedDeltaTime;
+
+        backTire.AddTorque(tireRotationTorque);
+        frontTire.AddTorque(tireRotationTorque);
+
         carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
 
+        if (parachuteDeployed)
+        {
+            float reductionFactor = 0.1f;
+
+            carRigidbody.velocity = new Vector2(
+                carRigidbody.velocity.x * (1 - reductionFactor * 1.2f),
+                carRigidbody.velocity.y * (1 - reductionFactor * 1.2f) 
+            );
+            tireRotationTorque += tireRotationReductionFactor;
+            backTire.angularVelocity *= Mathf.Clamp01(1 - tireRotationReductionFactor);
+            frontTire.angularVelocity *= Mathf.Clamp01(1 - tireRotationReductionFactor);
+
+            if (Mathf.Abs(backTire.angularVelocity) < 0.1f)
+            {
+                backTire.angularVelocity = 0f;
+            }
+            if (Mathf.Abs(frontTire.angularVelocity) < 0.1f)
+            {
+                frontTire.angularVelocity = 0f;
+            }
+
+            if (isGrounded)
+            {
+                DismantleParachute();
+            }
+        }
+        else
+        {
+            carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        }
     }
+
 }
